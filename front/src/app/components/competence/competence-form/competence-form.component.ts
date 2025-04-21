fda54b3ec9b47c867268d7c723c8477ca933a1e0
YosrMekkii <yosr.mekki@esprit.tn>
2025-04-22 00:40:26 +0100

    competence form

---
+++ front/src/app/components/competence/competence-form/competence-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-competence-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './competence-form.component.html',
+  styleUrl: './competence-form.component.scss'
+})
+export class CompetenceFormComponent {
+
+}
