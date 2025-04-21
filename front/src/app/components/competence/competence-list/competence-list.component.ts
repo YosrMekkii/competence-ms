cbb2e06adeebcd9f166312cfabaa2b17199ed959
YosrMekkii <yosr.mekki@esprit.tn>
2025-04-22 00:39:29 +0100

    generate component list

---
+++ front/src/app/components/competence/competence-list/competence-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-competence-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './competence-list.component.html',
+  styleUrl: './competence-list.component.scss'
+})
+export class CompetenceListComponent {
+
+}
